58e2cc0b8fd1466f8f37538c6d6f6aed4cec6a1c
marian <marianocarrazana@gmail.com>
2025-01-15 17:23:01 -0300

    Get theme from landing/dashboard if available

---
+++ satori_help/docs/.vitepress/theme/CustomLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import { provide } from 'vue'
+
+const { isDark } = useData()
+
+provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+  console.log(isDark);
+  isDark.value = !isDark.value;
+  localStorage.setItem("theme", isDark.value ? "dark" : "light");
+})
+
+// Share theme between landing/dashboard
+const savedTheme = localStorage.getItem('theme') ||
+  (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+console.log(savedTheme);
+isDark.value = savedTheme === 'dark';
+</script>
+
+<template>
+  <DefaultTheme.Layout />
+</template>
