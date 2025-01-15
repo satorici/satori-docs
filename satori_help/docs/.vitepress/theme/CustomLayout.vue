085d45eb7a8bf19baf3ce99d02ab4786b4676bda
marian <marianocarrazana@gmail.com>
2025-01-15 17:58:58 -0300

    Save/load theme from cookies if available

--- satori_help/docs/.vitepress/theme/CustomLayout.vue
+++ satori_help/docs/.vitepress/theme/CustomLayout.vue
@@ -5,13 +5,27 @@ import { provide } from 'vue'
 
 const { isDark } = useData()
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function setCookie(name, value) {
+  let date = new Date();
+  date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+  let expires = "; expires=" + date.toUTCString();
+  document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=.satori.ci";
+}
+
 provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
   isDark.value = !isDark.value;
-  localStorage.setItem("theme", isDark.value ? "dark" : "light");
+  // Save in a cookie to share the theme between subdomains
+  setCookie("theme", isDark.value ? "dark" : "light");
 })
 
 // Share theme between landing/dashboard
-const savedTheme = localStorage.getItem('theme') ||
+const savedTheme = getCookie('theme') ||
   (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 isDark.value = savedTheme === 'dark';
 </script>
